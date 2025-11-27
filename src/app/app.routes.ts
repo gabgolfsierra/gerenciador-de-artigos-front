583bfd08c53126a1679d3c9d22d72e400cce349d
Gabriel Santino <ggs2209@gmail.com>
2025-11-27 00:12:24 -0300

    Correção nome diretório

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ArtigoFormComponent } from './pages/artigos-list/artigos-form.component';
+import { ArtigoFormComponent } from './pages/artigos-form/artigos-form.component';
 import { LoginComponent } from './pages/auth/login.component';
 import { RegisterComponent } from './pages/auth/register.component';
 import { TrabalhoEventoFormComponent } from './pages/trabalhos-eventos/trabalho-evento-form.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // página inicial depois do login
+  { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'artigos', component: ArtigoFormComponent },
